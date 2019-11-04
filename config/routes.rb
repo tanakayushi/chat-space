089460fd1053f07cb7e3047b7933374350c4677b
tanakayushi <tanakaunagi@gmail.com>
2019-11-04 11:54:13 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "messages#index"
+  root to: 'groups#index'
   resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
-
+  resources :groups, only: [:new, :create, :edit, :update, :index]
+    resources :messages, only: [:index]
 end
