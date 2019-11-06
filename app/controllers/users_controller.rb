a3f4e86ca52bcff5228a0dbe3297d4956ce95589
tanakayushi <tanakaunagi@gmail.com>
2019-11-06 14:10:45 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  def index 
+  end
+
   def edit
   end
 
@@ -12,8 +15,7 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params 
+  def user_params
     params.require(:user).permit(:name, :email)
   end
-
 end
