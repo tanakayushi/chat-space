7bebb221e3305f7146957fd254652b769344da7c
tanakayushi <tanakaunagi@gmail.com>
2019-11-06 14:10:40 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
   before_action :set_group
-  
-  def index 
+
+  def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
   end
