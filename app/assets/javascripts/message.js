37cd4ffb3aa7d00e3154fb9fa2ef36c49abd9f6f
tanakayushi <tanakaunagi@gmail.com>
2019-11-07 14:31:25 +0900

    Create message.js

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,49 @@
+$(document).on('turbolinks:load', function(){
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.date}
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                      ${content}
+                    </p>
+                    ${img}
+                  </div>
+                </div>`
+  return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.new_message')[0].reset(''); 
+      $('html,body').animate({scrollTop:$('.messages')[0].scrollHeight});
+      return false;
+    })
+    .fail(function(data){
+      alert('エラー');
+    })
+    .always(function(data){
+      $('.form__submit').prop('disabled', false);
+    })
+  })
+});
