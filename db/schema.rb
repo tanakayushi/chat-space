4e210a178507e9797bd4e9f265f0096001061fb6
tanakayushi <tanakaunagi@gmail.com>
2019-11-11 03:05:52 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191104063758) do
+ActiveRecord::Schema.define(version: 20191108112306) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "group_id"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20191104063758) do
     t.datetime "updated_at",                          null: false
     t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
